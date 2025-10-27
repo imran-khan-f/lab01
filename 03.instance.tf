20e10eef19b94fa46e7dc2b81e7a997fad17d4bb
Imrankhan <imrankhan.f1307@gmail.com>
2025-10-28 03:21:20 +0530

    corrected the pem key

--- 03.instance.tf
+++ 03.instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "Myinstance-010" {
   #ami             = "ami-0d902a8756c37e690"
   ami           = var.aws-ec2-ami-id
   #ami           = data.aws_ami.amazon-ec2-ami.id
-  key_name      = "1316-us-west-1"
+  key_name      = "1307-us-west-1"
   instance_type = "t2.micro"
   #instance_type = var.aws-ec2-01
   #count           = var.ec2-instance-count
